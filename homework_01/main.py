4332102f44a1f0cf8dd24c6c654b6e7c6a947abc
Dmitryi Pletnev <dmpinform@gmail.com>
2021-03-12 00:29:34 +0300

    homework 1

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,11 +4,15 @@
 """
 
 
-def power_numbers():
+def power_numbers(numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     """
+    listn = list(num**2 for num in numbers if str(num).isdigit())
+    if len(listn) != len(numbers):
+        print(f'Error int value in list: {numbers}')
+    return listn
 
 
 # filter types
@@ -17,9 +21,31 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def filter_numbers(numbers, ft=ODD):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
     """
+    listn = list(num for num in numbers if str(num).isdigit())
+    if len(listn) != len(numbers):
+        print(f'Error int value in list: {numbers}')
+        return numbers
+
+    list_result = []
+    if ft == ODD:
+        list_result = list(n for n in numbers if n % 2 > 0)
+    if ft == EVEN:
+        list_result = list(n for n in numbers if n % 2 == 0)
+    if ft == PRIME:
+        for n in numbers:
+            isprime = True
+            if n > 1:
+                for x in range(2, n):
+                    if n % x == 0:
+                        isprime = False
+                        break
+                if isprime:
+                    list_result.append(n)
+
+    return list_result
