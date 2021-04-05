ca8eb619229a5ce39f68e42f52e143890d337b73
dmpinform <80347958+dmpinform@users.noreply.github.com>
2021-04-05 21:46:11 +0300

    Update __init__.py

--- homework_02/__init__.py
+++ homework_02/__init__.py
@@ -2,12 +2,6 @@
 Домашнее задание №2
 Классы и модули
 """
-import sys
-import os
-
-PACKAGE_PARENT = '..'
-SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
-sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from homework_02 import base, car, engine, exceptions, plane
 
