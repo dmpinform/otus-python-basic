cce64994100ee08f9e846254230a9627c72fbd1c
Dmitryi Pletnev <dmpinform@gmail.com>
2021-06-27 14:53:29 +0300

    hw08

--- homework_08/myproject/myproject/urls.py
+++ homework_08/myproject/myproject/urls.py
@@ -17,13 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from picpart.views import index_view, upgrade_view
+from picpart.views import index_view, detail_view, about_view, PictureListView, PictureDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view),
-    path('upgrade/', upgrade_view),
-
+    path('', PictureListView.as_view()),
+    path('picpart/<int:pk>/', PictureDetailView.as_view()),
+    path('about/', about_view),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- homework_08/myproject/picpart/views.py
+++ homework_08/myproject/picpart/views.py
@@ -3,25 +3,45 @@ from django.shortcuts import render
 from .models import Pictures, PictureUpgrade
 from .tasks import save_pictures_task
 from django.core.mail import send_mail
+from django.views.generic import ListView, DetailView
+
+
+'''На классах CBV generic'''
+
+
+class PictureListView(ListView):
+    model = Pictures
+    template_name = 'picpart/index.html'
+    # добавить пагинацию на странице
+    # paginate_by = 10
+
+
+class PictureDetailView(DetailView):
+    model = Pictures
+    template_name = 'picpart/detail.html'
+
+
+'''На функциях'''
 
 
 def index_view(request):
-
     pictures = Pictures.objects.all()
     if request.method == 'POST':
         # запустить RabbitMQ
         # save_pictures_task.delay()
         send_mail('Subject here', 'Here is the message.', 'from@example.com',
                   ['dmpinform@gmail.com'], fail_silently=False)
-
     return render(request, 'picpart/index.html', {'pictures': pictures})
 
 
-def upgrade_view(request):
+def detail_view(request):
     picture_id = request.GET["picture_id"]
     picture = Pictures.objects.get(pk=picture_id)
-    return render(request, 'picpart/upgrade.html', {'picture': picture})
+    return render(request, 'picpart/detail.html', {'picture': picture})
 
 
-def load_image_view(request):
-    return render(request, 'шаблон')
+def about_view(request):
+    return render(request, 'picpart/about.html')
+
+
+
