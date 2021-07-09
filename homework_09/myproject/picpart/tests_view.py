ebb0c37e369c7305131b4e4532523308eed582a2
Dmitryi Pletnev <dmpinform@gmail.com>
2021-07-09 21:48:36 +0300

    ok

--- homework_09/myproject/picpart/tests_view.py
+++ homework_09/myproject/picpart/tests_view.py
@@ -4,14 +4,33 @@ from .models import Pictures
 
 class TestViews(TestCase):
 
-    def test_views(self):
+    def test_index(self):
         response = self.client.get('/')
         # Проверка кода ответа
         self.assertEqual(response.status_code, 200)
 
         # Проверка контекста
         response = self.client.get('/')
-        self.assertEqual('object_list' in response.context, True)
         self.assertTrue('object_list' in response.context)
-        self.assertIn('object_list', response.context)
         self.assertEqual(len(response.context['object_list']), 0)
+
+    def test_about(self):
+        response = self.client.get('/about/')
+        # Проверка кода ответа
+        self.assertEqual(response.status_code, 200)
+
+        # Проверка контекста
+        response = self.client.get('/')
+        self.assertTrue('object_list' in response.context)
+        self.assertEqual(len(response.context['object_list']), 0)
+
+    def test_create(self):
+        response = self.client.get('/picpart/create/')
+        # Проверка кода ответа
+        self.assertEqual(response.status_code, 200)
+
+        # Проверка контекста
+        response = self.client.get('/')
+        self.assertTrue('object_list' in response.context)
+        self.assertEqual(len(response.context['object_list']), 0)
+
